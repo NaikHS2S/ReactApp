1c9181865323e6c4da5392ad5c7aaaf485a61efb
NaikHS2S <NaikH988@Outlook.com>
2022-02-19 11:37:25 +0530

    update

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,12 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-import App from '../src/screens/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import LoginScreen from '../src/screens/LoginScreen';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<LoginScreen />);
 });
