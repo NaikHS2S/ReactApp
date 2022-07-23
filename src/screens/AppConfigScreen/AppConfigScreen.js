051e4cccdf698092f7154f03fef1a0a9bb7c1740
Harekrishna Naik <hknaik988@Harekrishnas-MacBook-Air.local>
2022-07-23 21:42:18 +0530

    data updated

--- src/screens/AppConfigScreen/AppConfigScreen.js
+++ src/screens/AppConfigScreen/AppConfigScreen.js
@@ -19,6 +19,8 @@ const {emailId} = useSelector( (state) => state.loginAppReducer)
       navigation.replace('Second', {
         emailId: emailId,
       });
+    }else{
+      navigation.replace('Login');
     }
   }, []);
 
