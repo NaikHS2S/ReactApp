7efe37d716e82f78210799250d4cf21662add340
NaikHS2S <NaikH988@Outlook.com>
2022-07-23 16:18:19 +0530

    updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,30 @@
-import { createStore } from 'redux'
-import rootReducer from './reducer'
+import { configureStore } from '@reduxjs/toolkit'
+import thunk from "redux-thunk";
+import rootReducer from './reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const store = createStore(rootReducer)
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    transforms: [
+        encryptTransform({
+          secretKey: 'my-super-secret-key',
+          onError: function (error) {
+            // Handle the error.
+          },
+        }),
+      ],
+  }
+   
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+   
+  export const store = configureStore({
+    reducer: {
+        loginReducer: persistedReducer,
+    },
+    middleware: [thunk]
+  })
 
-export default store
+  export const persistor = persistStore(store)
